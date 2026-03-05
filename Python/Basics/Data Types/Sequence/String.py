6e5b2ec92545f5f0f42adc46effe4c3afa267ad2
JALAL <jalaluddin0046356@gmail.com>
2026-03-06 03:19:10 +0600

    Data Type

---
+++ Python/Basics/Data Types/Sequence/String.py
@@ -0,0 +1,10 @@
+s = 'Welcome to the Geeks World'
+print(s)
+
+# check data type 
+print(type(s))
+
+# access string with index
+print(s[1])
+print(s[2])
+print(s[-1]) # -1 refers to the last character, -2 is second last, and so on
